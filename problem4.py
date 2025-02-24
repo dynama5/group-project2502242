0891bb31ecfc8e4426970439954d4f6c39b4b6d0
dynama5 <155870827+dynama5@users.noreply.github.com>
2025-02-24 23:44:51 +0000

    solved problem 4

---
+++ problem4.py
@@ -0,0 +1,21 @@
+# Given an integer n, return a string array answer where:
+
+# answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+# answer[i] == "Fizz" if i is divisible by 3.
+# answer[i] == "Buzz" if i is divisible by 5.
+# answer[i] == i (as a string) if none of the above conditions are true.
+
+def fizzBuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+fizzBuzz(15)
+fizzBuzz(12)
+fizzBuzz(20)
+fizzBuzz(17)
